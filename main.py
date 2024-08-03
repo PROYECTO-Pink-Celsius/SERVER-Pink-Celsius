2d7af5001611de6fdf60d33331e7969548ff56b8
nyascla <lavernia.llorens@gmail.com>
2024-08-03 17:55:57 +0200

    ahora la vista se distribuye desde el propio server

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+
 from src.routes import data
 from src.data.database import engine
 
 app = FastAPI()
+app.include_router(data.router)
 
 
 # Crear las tablas en la base de datos
@@ -10,13 +14,22 @@ def create_tables():
     from src.data.models import Base
     Base.metadata.create_all(bind=engine)
 
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
 @app.on_event("startup")
 def startup_event():
     create_tables()
 
 # Registrar las rutas
-app.include_router(data.router)
 
 
-# uvicorn src.main:app --reload
-# uvicorn src.main:app --host 192.168.1.200 --port 8000
+# uvicorn main:app --reload
+# uvicorn main:app --host 192.168.1.127 --port 8000
